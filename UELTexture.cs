a82c896802c8eff332a9b2cbc584952b2c126902
Charles 'TickTakashi' Gbadamosi <ctg11@imperial.ac.uk>
2015-12-31 12:53:22 +0000

    Adds UELTexture, an adapter wrapper class for Image and SpriteRenderer

---
+++ UELTexture.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+namespace UEL {
+  public abstract class UELTexture {
+    public abstract Color color { get; set; }
+    public abstract T GetComponent<T>();
+  }
+
+  public class UELImage : UELTexture {
+    Image img;
+    public UELImage(Image img) { this.img = img; }
+    public override Color color { get { return img.color; } set { img.color = value; } }
+
+    public override T GetComponent<T>() {
+      return img.GetComponent<T>();
+    }
+  }
+
+  public class UELSpriteRenderer : UELTexture {
+    SpriteRenderer spr;
+    public UELSpriteRenderer(SpriteRenderer spr) { this.spr = spr; }
+    public override Color color { get { return spr.color; } set { spr.color = value; } }
+
+    public override T GetComponent<T>() {
+      return spr.GetComponent<T>();
+    }
+  }
+}
